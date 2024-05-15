33d73b9ed3e1238b10a20df2bc04b726e4a6fd62
Thomas <thomas.zielasny@syncorenize.com>
2024-05-15 21:04:09 +0200

    readme and screenshots changed

--- cmd/main.go
+++ cmd/main.go
@@ -9,8 +9,8 @@ import (
 )
 
 func main() {
-	startingCash := 250 // modify the starting cash, for simulations with 100000 spins you can set it 100000 to get more granularity
 	currency := ""
+	startingCash := 250                   // modify the starting cash, for simulations with 100000 spins you can set it 100000 to get more granularity
 	visualDelay := 100 * time.Millisecond // refreshing rate of the symbols, for testing/simulations you might want to set it to 0
 	autoDelay := 1 * time.Second          // delay between the auto spins, for testing/simulations you might want to set it to 0
 	runSim := false                       // set to true to run the simulation
@@ -21,19 +21,23 @@ func main() {
 	game := wildfruits.NewSlotMachine(rng, visualDelay, startingCash, currency)
 
 	disp := &display.ConsoleDisplay{}
-	game.RegisterObserver(disp) // I suggest to comment this line out for simulations since it produces the console output and slows down simulations
-
-	m := manager.GameManager{
-		Game:      game,
-		Display:   disp,
-		AutoDelay: autoDelay,
-	}
 
 	if runSim {
+		m := manager.GameManager{
+			Game:      game,
+			Display:   disp,
+			AutoDelay: autoDelay,
+		}
 		totalSpins := 100000
 		betAmount := 1
 		m.RunSimulation(totalSpins, betAmount)
 	} else {
+		game.RegisterObserver(disp)
+		m := manager.GameManager{
+			Game:      game,
+			Display:   disp,
+			AutoDelay: autoDelay,
+		}
 		m.Play()
 	}
 }
